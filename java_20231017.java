6323e525b7fad94c78f7d040a5083851686f21f3
midoriSJ <106817383+midoriSJ@users.noreply.github.com>
2023-10-17 21:46:14 +0900

    20231017

--- java_20231017.java
+++ java_20231017.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 
 public class java_20231017 {
 
@@ -27,6 +29,7 @@ public class java_20231017 {
 		
 		// char = 'A';(캐릭터 한글자만 표현)
 		
+		/*
 		int alphabetA = 'A';
 		int alphabetB = 'B';
 		char alphabetC = 'C';
@@ -48,7 +51,57 @@ public class java_20231017 {
 		else {
 			System.out.println("학생이 아닙니다");
 			// student = false일 시 학생이 아님 출력
-		}
+			 
+		*/
+		
+		// 자동 형 변환
+		// char -> short -> int -> long
+		/*
+		int num1 = 14;
+		double num2 = num1;
+		System.out.println(num2);
+		*/
+		/*
+		int num3 = 10;
+		byte num4 = (byte) num3;
+		System.out.println(num4); // 손실이 없다 -> byte 값의 범위 안에 있기 때문
+		
+		int num5 = 1000;
+		byte num6 = (byte) num5;  // 8bit = 2^7 = 128-1 = 127
+		System.out.println(num6); // 손실이 있다 -> byte 값의 범위 안에 없기 때문
+		
+		*/
+		
+		//자바 라이브러리 import
+		//import java.util.Scanner;
+		//Scanner 객체 선언
+		//Scanner input = new Scanner(System.in);
+		//값을 입력받기
+		
+		// byte input_num = input.nextByte();
+		// 정수형 : byte nextByte() : 입력 받을 값을 byte형으로 변환
+		// short int long도 위와 동일
+		// 실수형 : float nextFloat() : 입력 받을 값을 float형으로 변환
+		// double도 위와 동일
+		// 문자형 : String nextLine() : 입력받은 전체의 문자열 타입을 엔터구분
+		// String next() : 입력 받을 문자열 타입으로 (띄어쓰기)
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("이름 주소 나이 체중을 빈칸으로 구분해서 입력받고 출력하기\n");
+		
+	    String name, address;
+	    int age;
+	    double weight;
+	    
+	    name = sc.next();
+	    address = sc.next();
+	    age = sc.nextInt();
+	    weight = sc.nextDouble();
+	    
+	    System.out.println("당신의 이름은 " + name + "입니다.");
+	    System.out.println("당신의 주소는 " + address + "입니다.");
+	    System.out.println("당신의 나이는 " + age + "입니다.");
+	    System.out.println("당신의 체중은 " + weight + "입니다.");
 	}
-
 }
